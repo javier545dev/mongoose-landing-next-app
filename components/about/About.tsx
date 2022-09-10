ec8e231217b8c79bff208b662d776c627cb9a3e1
Javier Fuentes <javierfuentes545@gmail.com>
2022-09-09 22:47:27 -0500

    [fix] route name crash

--- components/about/About.tsx
+++ components/about/About.tsx
@@ -3,7 +3,7 @@ export const About = () => {
     <div className='min-w-screen hero min-h-screen bg-base-200 pt-[7vh] md:pt-[15vh]'>
       <div className='hero-content text-center'>
         <div className='max-w-lg'>
-          <h2 className='py-5 text-5xl font-bold text-secondary'>About us?</h2>
+          <h2 className='py-5 text-5xl font-bold text-secondary'>About Us?</h2>
           <p className='py-6 text-justify px-6'>
             We are a 100% Salvadoran financial technology company focused on cryptocurrencies, we seek to facilitate these technologies to all people, designing high-quality and easy-to-understand software, we also educate people about what they are and how to manage crypto assets.
           </p>
