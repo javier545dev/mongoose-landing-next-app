8e39ec9ca43bfb8824a1db426a67522367c3df1f
Javier Fuentes <javierfuentes545@gmail.com>
2022-09-12 21:25:36 -0500

    [add] seo/head html in indexComponent

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import { Layout } from '../components/Layout/Layout'
 import { Hero } from '../components/hero/Hero'
 import { Course } from '../components/course/Course'
@@ -7,6 +8,27 @@ import { About as AboutComponent } from '../components/about/About'
 export default function HomePage() {
   return (
     <>
+      <Head>
+
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          content="text/html; charset=UTF-8; X-Content-Type-Options=nosniff"
+          httpEquiv="Content-Type"
+        />
+        <link rel="apple-touch-icon" href="/favicon.ico" />
+        <meta name="description" content="InstaPet" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="http://example.com/images/image.jpg" />
+
+        <title>Mongoose FinTech</title>
+        <meta property="og:title" content="Mongoose" />
+        <meta property="og:site_name" content="MongooseTech" />
+        <meta property="og:url" content="https://mongoose-tech.vercel.app" />
+        <meta property="og:locale" content="es_LA" />
+        <meta name="description" content="Simplifying access to cryptocurrencies and financial inclusion..." />
+
+      </Head>
       <Layout>
         <Hero />
         <Course />
